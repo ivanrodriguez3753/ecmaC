ba83c60e28ff258771c68cc8f3fef69232de4827
Ivan Rodriguez <ivanrodriguez3753@gmail.com>
2022-02-05 20:09:29 -0600

    Added tokens

---
+++ README.md
@@ -0,0 +1,4 @@
+Not a real readme thusfar but a place for our notes so far.
+
+We're going off the 2022 ECMAScript Spec, currently found at: https://tc39.es/ecma262/
+
--- src/lexer.h
+++ src/lexer.h
@@ -2,6 +2,50 @@
 #define ECMAC_LEXER_H
 
 #include <iostream>
+
+enum TOKS {
+    //identifiers
+    ID,
+    //literals
+    NUM_LITERAL,
+    STR_LITERAL,
+    //keywords
+    AWAIT_KW,
+    BREAK_KW,
+    CASE_KW,
+    CATCH_KW,
+    CLASS_KW,
+    CONST_KW,
+    CONTINUE_KW,
+    DEBUGGER_KW,
+    DEFAULT_KW,
+    DELETE_KW,
+    DO_KW,
+    ELSE_KW,
+    EXPORT_KW,
+    EXTENDS_KW,
+    FINALLY_KW,
+    FOR_KW,
+    FUNCTION_KW,
+    IF_KW,
+    IMPORT_KW,
+    IN_KW,
+    INSTACEOF_KW,
+    NEW_KW,
+    RETURN_KW,
+    SUPER_KW,
+    SWITCH_KW,
+    THIS_KW,
+    THROW_KW,
+    TRY_KW,
+    TYPEOF_KW,
+    VAR_KW,
+    VOID_KW,
+    WHILE_KW,
+    WITH_KW,
+    YIELD_KW
+};
+
 class someClass {
 public:
     someClass() {
